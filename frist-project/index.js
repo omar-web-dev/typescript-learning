4d793be9fdb83e8e170f3ae3d08fba2e849e636f
omarwebdeve <omarf977865@gmail.com>
2022-12-26 07:39:17 +0600

    first commit

---
+++ frist-project/index.js
@@ -0,0 +1,5 @@
+var userName;
+userName = "Omar Faruk";
+var age = 25;
+var userPassword = "Omar234";
+console.log(userName, age, userPassword);
---
+++ frist-project/index.ts
@@ -0,0 +1,5 @@
+let userName : string
+userName = "Omar Faruk";
+let age = 25
+let userPassword = "Omar234";
+console.log(userName, age,userPassword)
