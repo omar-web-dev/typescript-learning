3fa0ecca246ebf7fba57933df2a8aa032f04619e
omarwebdeve <omarf977865@gmail.com>
2022-12-26 09:07:53 +0600

    object-type

---
+++ user-defi-type/object-type.js
@@ -0,0 +1,19 @@
+var users;
+users = [
+    {
+        userName: 'Omar',
+        age: 21,
+        cell: "01970299035"
+    },
+    {
+        userName: 'Mamun',
+        age: 22,
+        cell: "01970299035"
+    },
+    {
+        userName: 'Tanveer',
+        age: 23,
+        cell: "01970299035"
+    },
+];
+console.log(users);
---
+++ user-defi-type/object-type.ts
@@ -0,0 +1,21 @@
+let users : object[]
+
+users = [
+    {
+        userName : 'Omar',
+        age : 21,
+        cell : "01970299035"
+    },
+    {
+        userName : 'Mamun',
+        age : 22,
+        cell : "01970299035"
+    },
+    {
+        userName : 'Tanveer',
+        age : 23,
+        cell : "01970299035"
+    },
+]
+
+console.log(users)
